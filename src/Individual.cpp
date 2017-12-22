267b550d36016882424ee8a87d162e9d681b63fe
N1K1T4 <nikita.mauvisseau@hotmail.fr>
2017-12-22 11:25:06 +0100

    individual compiling

--- src/Individual.cpp
+++ src/Individual.cpp
@@ -1,35 +1,35 @@
-#include "Individual.cpp"
+#include "Individual.h"
 #include <random>
 
-double Individual::mean = 5;
-double Individual::p = 0.5;
+double Individual::_mean = 5;
+double Individual::_p = 0.5;
 
 Individual::Individual(const Individual& indiv)
 {
-  size = indiv.size();
-  n_genes = indiv.n_genes();
-  genes = new unsigned[2 * n_genes];
-  for (unsigned* i = genes, j = indiv.genes(); i < genes + 2 * n_genes; ++i, ++j)
+  _size = indiv.get_size();
+  _n_genes = indiv.get_n_genes();
+  _genes = new unsigned[2 * _n_genes];
+  for (unsigned *i = _genes, *j = indiv.get_genes(); i < _genes + 2 * _n_genes; ++i, ++j)
     *i = *j;
-  n_barriers = indiv.n_barriers();
-  barriers = new unsigned[n_barriers];
-  for (unsigned* i = barriers, j = indiv.barriers(); i < barriers + n_barriers; ++i, ++j)
+  _n_barriers = indiv.get_n_barriers();
+  _barriers = new unsigned[_n_barriers];
+  for (unsigned *i = _barriers, *j = indiv.get_barriers(); i < _barriers + _n_barriers; ++i, ++j)
     *i = *j;
-  fitness = indiv.fitness();
+  _fitness = indiv.get_fitness();
 }
 
 Individual::Individual(Individual&& indiv)
 {
-  size = indiv.size();
-  n_genes = indiv.n_genes();
-  genes = new unsigned[2 * n_genes];
-  for (unsigned* i = genes, j = indiv.genes(); i < genes + 2 * n_genes; ++i, ++j)
+  _size = indiv.get_size();
+  _n_genes = indiv.get_n_genes();
+  _genes = new unsigned[2 * _n_genes];
+  for (unsigned *i = _genes, *j = indiv.get_genes(); i < _genes + 2 * _n_genes; ++i, ++j)
     *i = *j;
-  n_barriers = indiv.n_barriers();
-  barriers = new unsigned[n_barriers];
-  for (unsigned* i = barriers, j = indiv.barriers(); i < barriers + n_barriers; ++i, ++j)
+  _n_barriers = indiv.get_n_barriers();
+  _barriers = new unsigned[_n_barriers];
+  for (unsigned *i = _barriers, *j = indiv.get_barriers(); i < _barriers + _n_barriers; ++i, ++j)
     *i = *j;
-  fitness = indiv.fitness();
+  _fitness = indiv.get_fitness();
 	indiv.reset();
 }
 
@@ -40,34 +40,34 @@ Individual::Individual(unsigned size,
                        unsigned* barriers,
                        float fitness)
 {
-  this->size = size;
-  this->n_genes = n_genes;
-  this->genes = new unsigned[2 * n_genes];
-  for (unsigned* i = this->genes, j = genes; i < genes + 2 * n_genes; ++i, ++j)
+  _size = size;
+  _n_genes = n_genes;
+  _genes = new unsigned[2 * _n_genes];
+  for (unsigned *i = _genes, *j = genes; i < _genes + 2 * _n_genes; ++i, ++j)
     *i = *j;
-  this->n_barriers = n_barriers;
-  barriers = new unsigned[n_barriers];
-  for (unsigned* i = this->barriers, j = barriers; i < barriers + n_barriers; ++i, ++j)
+  _n_barriers = n_barriers;
+  _barriers = new unsigned[_n_barriers];
+  for (unsigned *i = _barriers, *j = barriers; i < _barriers + _n_barriers; ++i, ++j)
     *i = *j;
-  this->fitness = fitness;
+  _fitness = fitness;
 }
 
 Individual& Individual::operator=(const Individual& indiv)
 {
 	if (this != &indiv)
 	{
-	  size = indiv.size();
-    n_genes = indiv.n_genes();
-    delete[] genes;
-    genes = new unsigned[2 * n_genes];
-    for (unsigned* i = genes, j = indiv.genes(); i < genes + 2 * n_genes; ++i, ++j)
+    _size = indiv.get_size();
+    _n_genes = indiv.get_n_genes();
+    delete[] _genes;
+    _genes = new unsigned[2 * _n_genes];
+    for (unsigned *i = _genes, *j = indiv.get_genes(); i < _genes + 2 * _n_genes; ++i, ++j)
       *i = *j;
-    n_barriers = indiv.n_barriers();
-    delete[] barriers;
-    barriers = new unsigned[n_barriers];
-    for (unsigned* i = barriers, j = indiv.barriers(); i < barriers + n_barriers; ++i, ++j)
+    _n_barriers = indiv.get_n_barriers();
+    delete[] _barriers;
+    _barriers = new unsigned[_n_barriers];
+    for (unsigned *i = _barriers, *j = indiv.get_barriers(); i < _barriers + _n_barriers; ++i, ++j)
       *i = *j;
-    fitness = indiv.fitness();
+    _fitness = indiv.get_fitness();
 	}
 	return *this;
 }
@@ -76,18 +76,18 @@ Individual& Individual::operator=(Individual&& indiv)
 {
 	if (this != &indiv)
 	{
-	  size = indiv.size();
-    n_genes = indiv.n_genes();
-    delete[] genes;
-    genes = new unsigned[2 * n_genes];
-    for (unsigned* i = genes, j = indiv.genes(); i < genes + 2 * n_genes; ++i, ++j)
+    _size = indiv.get_size();
+    _n_genes = indiv.get_n_genes();
+    delete[] _genes;
+    _genes = new unsigned[2 * _n_genes];
+    for (unsigned *i = _genes, *j = indiv.get_genes(); i < _genes + 2 * _n_genes; ++i, ++j)
       *i = *j;
-    n_barriers = indiv.n_barriers();
-    delete[] barriers;
-    barriers = new unsigned[n_barriers];
-    for (unsigned* i = barriers, j = indiv.barriers(); i < barriers + n_barriers; ++i, ++j)
+    _n_barriers = indiv.get_n_barriers();
+    delete[] _barriers;
+    _barriers = new unsigned[_n_barriers];
+    for (unsigned *i = _barriers, *j = indiv.get_barriers(); i < _barriers + _n_barriers; ++i, ++j)
       *i = *j;
-    fitness = indiv.fitness();
+    _fitness = indiv.get_fitness();
 		indiv.reset();
 	}
 	return *this;
@@ -95,31 +95,29 @@ Individual& Individual::operator=(Individual&& indiv)
 
 void Individual::set_mutation(double indel_nb, double inv_p)
 {
-  mean = indel_nb;
-  p = inv_p;
+  _mean = indel_nb;
+  _p = inv_p;
 }
 
 void Individual::mutate(void)
 {
   
-  return void;
 }
 
 void Individual::reset(void)
 {
-  size = 0;
-  n_genes = 0;
-  n_barriers = 0;
-  delete genes;
-  genes = nullptr;
-  delete barriers
-  barriers = nullptr;
-  fitness = 0;
-  return void;
+  _size = 0;
+  _n_genes = 0;
+  _n_barriers = 0;
+  delete _genes;
+  _genes = nullptr;
+  delete _barriers;
+  _barriers = nullptr;
+  _fitness = 0;
 }
 
 Individual::~Individual(void)
 {
-  delete[] genes;
-  delete[] barriers
+  delete[] _genes;
+  delete[] _barriers;
 }
--- src/Individual.h
+++ src/Individual.h
@@ -4,14 +4,14 @@
 class Individual
 {
   private:
-    static double mean; // poisson distribution parameter for indels
-    static double p; // probability for an inversion to occur during replication
-    unsigned size{};
-    unsigned n_genes{};
-    unsigned* genes{}; // even: start; uneven: end
-    unsigned n_barriers{};
-    unsigned* barriers{};
-    float fitness{};
+    static double _mean; // poisson distribution parameter for indels
+    static double _p; // probability for an inversion to occur during replication
+    unsigned _size{};
+    unsigned _n_genes{};
+    unsigned* _genes{}; // even: start; uneven: end
+    unsigned _n_barriers{};
+    unsigned* _barriers{};
+    float _fitness{};
   public:
     Individual(void) {} // default constructor
     Individual(const Individual& indiv); // copy constructor
@@ -24,18 +24,18 @@ class Individual
                float fitness);
     Individual& operator=(const Individual& indiv); // copy assignment operator
     Individual& operator=(Individual&& indiv); // move assignment operator
-    static double mean(void) {return mean;}
-    static double p(void) {return p;}
-    static void set_mutation(double indel_mean, double inv_p);
-    unsigned size(void) {return size;}
-    unsigned n_genes(void) {return n_genes;}
-    unsigned* genes(void) {return genes;}
-    unsigned gene_start(unsigned index) {return genes[2*index];}
-    unsigned gene_end(unsigned index) {return genes[2*index+1];}
-    unsigned n_barriers(void) {return n_barriers;}
-    unsigned* barriers(void) {return barriers;}
-    unsigned barrier(unsigned index) {return barriers[index];}
-    float fitness(void) {return fitness;}
+    static double get_mean(void) {return _mean;}
+    static double get_p(void) {return _p;}
+    static void set_mutation(double mean, double p);
+    unsigned get_size(void) const {return _size;}
+    unsigned get_n_genes(void) const {return _n_genes;}
+    unsigned* get_genes(void) const {return _genes;}
+    unsigned gene_start(unsigned index) const {return _genes[2*index];}
+    unsigned gene_end(unsigned index) const {return _genes[2*index+1];}
+    unsigned get_n_barriers(void) const {return _n_barriers;}
+    unsigned* get_barriers(void) const {return _barriers;}
+    unsigned barrier(unsigned index) const {return _barriers[index];}
+    float get_fitness(void) const {return _fitness;}
     void mutate(void);
     void reset(void);
     ~Individual(void); // destructor
